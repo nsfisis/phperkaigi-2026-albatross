6bd35e345a4c5d74578b0f8a5c969027e7e15f02
nsfisis <nsfisis@gmail.com>
2026-02-14 11:31:02 +0900

    feat(frontend): add rollup-plugin-visualizer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	base: process.env.ALBATROSS_BASE_PATH || "/",
-	plugins: [tailwindcss(), react(), tsconfigPaths()],
+	plugins: [tailwindcss(), react(), tsconfigPaths(), visualizer()],
 });
